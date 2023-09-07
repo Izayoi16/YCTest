98daff039a1fc1b52ad787a8005efe942d596244
Juro Hsu <juro.hsu@Elu.com.tw>
2023-09-08 00:21:09 +0800

    Over

---
+++ Controllers/APIController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static YCTest.Models.YC;
+namespace Juro_Sales.Controllers
+{
+    [Route("[controller]")]
+    public class APIController : Controller
+    {
+        private readonly YCDbContext _db;
+        public APIController(YCDbContext dbComtext)
+        {
+            _db = dbComtext;
+        }
+        #region SalesAPI
+        [HttpGet("Saless")]
+        public IActionResult SalesRead(int page = 1) => Ok(_db.Sales.Skip((page - 1) * 10).Take(10));
+
+        [HttpGet("Sales")]
+        public IActionResult SalesReadDetail(string SalesCode) => Ok(_db.Sales.Find(SalesCode));
+        [HttpPost("Sales")]
+        public IActionResult SalesCreate(Sales data) => CreateData(data);
+        [HttpPut("Sales")]
+        public IActionResult SalesUpdate(Sales data) => Update(data);
+        [HttpDelete("Sales")]
+        public IActionResult SalesDelete(int id) => Delete(id, _db.Sales, "Sales");
+        #endregion
+
+        #region HousesAPI
+        [HttpGet("Houses")]
+        public IActionResult HouseRead(int page = 1) => Ok(_db.House.Skip((page - 1) * 10).Take(10));
+
+        [HttpGet("House")]
+        public IActionResult HouseReadDetail(string HouseCode) => Ok(_db.House.Find(HouseCode));
+        [HttpPost("House")]
+        public IActionResult HouseCreate(House data) => CreateData(data);
+        [HttpPut("House")]
+        public IActionResult HouseUpdate(House data) => Update(data);
+        [HttpDelete("House")]
+        public IActionResult HouseDelete(int id) => Delete(id, _db.House, "House");
+        #endregion
+
+        #region 泛型Function
+        private IActionResult CreateData<T>(T entity) where T : class
+        {
+            try
+            {
+                _db.Set<T>().Add(entity);
+                _db.SaveChanges();
+                return Created("Create Ok!", entity);
+            }
+            catch(Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+        private IActionResult Update<T>(T entity) where T : class
+        {
+            _db.Entry(entity).State = EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Conflict($"已被其他狀態更改：{ex.Message}");
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest($"錯誤：{ex.Message}");
+            }
+
+            return NoContent();
+        }
+        private IActionResult Delete<T>(int id, DbSet<T> dbSet, string entityName) where T : class
+        {
+            var data = dbSet.Find(id);
+
+            if (data == null)
+            {
+                return NotFound($"{entityName} 不存在 {id}");
+            }
+            dbSet.Remove(data);
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"錯誤：{ex.Message}");
+            }
+
+            return NoContent();
+        }
+        #endregion
+    }
+}
+
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using YCTest.Models;
+using static YCTest.Models.YC;
+using static YCTest.Models.DTO;
 
-namespace YCTest.Controllers
+namespace YCTest.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly YCDbContext _db;
+
+    public HomeController(YCDbContext dbComtext)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        _db = dbComtext;
     }
+
+    public IActionResult Index() => View(_db.House.ToList());
+    public IActionResult House()
+    {
+        ViewBag.sales = _db.Sales.ToList();
+        return View();
+    }
+    public IActionResult HouseDetail(int id)
+    {
+        ViewBag.sales = _db.Sales.ToList();
+        return View(_db.House.Find(id));
+    }
+    public IActionResult Sales() => View();
+    public IActionResult SalesDetail(Guid id) => View(_db.Sales.Find(id));
+    public IActionResult Detail(int id)
+    {
+        var surce = _db.House.Include(h => h.AttributionSales).SingleOrDefault(h => h.Id == id);
+        var data = new YCPriview
+        {
+            Title = surce?.Title,
+            Local = surce?.Local,
+            Master = surce?.Master,
+            MasterPhone = surce?.MasterPhone,
+            SelseName = surce?.AttributionSales?.Name,
+            SelsePhoneNumber = surce?.AttributionSales?.PhoneNumber,
+            Sid = surce?.AttributionSales?.Id ?? Guid.Empty,
+            Hid = surce?.Id ?? 0,
+            Price = surce?.Price ?? 0
+        };
+        return View(data);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
 }
